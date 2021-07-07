c7764d43b4585c19dd47691e4e6c0267c7cc4b49
Gaspare Mascolino <gaspare.mascolino@unicredit.eu>
2021-07-07 22:15:06 +0200

    null

--- JAVASCRIPT/upper-case.js
+++
@@ -1,7 +0,0 @@
-String.prototype.toJadenCase = function () {
-  console.log(this)
-    return this.split(" ").map( a => a[0].toUpperCase() + a.slice(1)).join(" ");
-  };
-
-var str = "How can mirrors be real if our eyes aren't real";
-console.log(str.toJadenCase());
