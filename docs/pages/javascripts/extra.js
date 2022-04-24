043e917ec6ae660fad23c0dc957f06aa648fe504
c333797 <c333797>
2022-04-24 17:35:07 +0200

    play and pause button

--- docs/pages/javascripts/extra.js
+++ docs/pages/javascripts/extra.js
@@ -1,23 +1,68 @@
-let y = 100;
+let y;
 let timer;
+
+const diesisChords = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+const bemolleChords = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B']
 
 const playButton = document.getElementById('play');
 const pauseButton = document.getElementById('pause');
 
+const range = document.getElementById('range');
+const rangeValue = document.getElementById('rangeValue');
+
+const transpose = document.getElementById('transpose');
+const transposeValue = document.getElementById('transposeValue');
+
 function start(start) {
-    if(start) {
-        playButton.style.visibility = "hidden"
-        pauseButton.style.visibility = "visible"
-        timer=setInterval(play, 50);
-    }
-    else {
-        playButton.style.visibility = "visible"
-        pauseButton.style.visibility = "hidden"
+    if (start) {
+        playButton.style.visibility = "hidden";
+        pauseButton.style.visibility = "visible";
+        pauseButton.style.visibility = "visible";
+        range.style.visibility = "hidden";
+        rangeValue.style.visibility = "hidden";
+        transpose.style.visibility = "hidden";
+
+        y = 0;
+        timer = setInterval(play, 50);
+    } else {
+        playButton.style.visibility = "visible";
+        pauseButton.style.visibility = "hidden";
+        range.style.visibility = "visible";
+        rangeValue.style.visibility = "visible";
+        transpose.style.visibility = "visible";
+
         clearInterval(timer);
     }
 }
 
 function play() {
-    window.scroll(0, y);
-    y += 0.5;
+    window.scroll(-100, y);
+    y += 0.5 * rangeValue.value;
+}
+
+function updateRangeInput(val) {
+    rangeValue.value = val;
+}
+
+function changeKey(value) {
+
+    transposeValue.value = String(Number(transposeValue.value)+value);
+
+    let songChords = document.getElementsByTagName('chord')
+    for (let i = 0; i < songChords.length; i++) {
+
+        let chord = songChords[i].innerHTML;
+
+        if (diesisChords.indexOf(chord) === 0 && value < 0) {
+            songChords[i].innerHTML = diesisChords[diesisChords.length - 1]
+        } else if(diesisChords.indexOf(chord) === diesisChords.length - value && value > 0) {
+            songChords[i].innerHTML = diesisChords[0]
+        } else if (chord.match('#')) {
+            songChords[i].innerHTML = diesisChords[diesisChords.indexOf(chord) + value]
+        } else if (chord.match('b')) {
+            songChords[i].innerHTML = bemolleChords[bemolleChords.indexOf(chord) + value]
+        } else {
+            songChords[i].innerHTML = diesisChords[diesisChords.indexOf(chord) + value]
+        }
+    }
 }
--- docs/pages/repertoire/converter.js
+++ docs/pages/repertoire/converter.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const fs = require('fs');
 
+const CHORD_REGEX= new RegExp('[CDEFGAB](?:#|b|)', 'g')
+
 fs.readdir(
     path.resolve(__dirname, 'lyrics'),
     (err, files) => {
@@ -21,7 +23,15 @@ fs.readdir(
                 list[3] = '<pre>\n'
 
                 for (let i = 4; i < list.length-1; i++) {
+
                     if(i%2===0) {
+                        let matches = list[i].match(CHORD_REGEX)
+
+                        if(matches) {
+                            matches.forEach( match => {
+                                list[i] = list[i].replace(match, '<chord>'+match+'</chord>')
+                            })
+                        }
                         list[i] = '<b>'+list[i]+'</b>'
                     }
                 }
@@ -31,7 +41,18 @@ fs.readdir(
                 list.push('<button id="play" class="md-button-play" onclick="start(true)">![](../assets/images/site/play.png){: style="height:50px;width:50px"}</button>')
                 list.push('<button id="pause" class="md-button-pause" onclick="start(false)">![](../assets/images/site/pause.png){: style="height:50px;width:50px"}</button>')
 
+                list.push('<input type="range" id="range" class="md-range" value="1" min="1" max="10" onchange="updateRangeInput(this.value);"/>')
+                list.push('<input type="text" id="rangeValue" class="md-range-value" value="1" readonly/>')
+
+                list.push('<div id="transpose">')
+                list.push('    <p id="transposeText"><b>Transpose:</b></p>')
+                list.push('    <input type="text" id="transposeValue" value="0" readonly/>')
+                list.push('    <button id="keyUp" class="md-buttonKey" onclick="changeKey(1)">+1</button>')
+                list.push('    <button id="keyDown" class="md-buttonKey" onclick="changeKey(-1)">-1</button>')
+                list.push('</div>')
+
                 console.log(list)
+
                 fs.writeFile(path.resolve(__dirname, file+'.md'), list.join('\n'), function(err) {
                     if(err) {
                         return console.log(err);
